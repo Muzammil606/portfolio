cdc740abe69e352f59084c0d9bc0f75450810ea6
Muzammil606 <150831372+Muzammil606@users.noreply.github.com>
2024-06-20 10:36:55 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,19 @@ export default function Home() {
     <main>
       <section className="relative w-screen h-screen flex md:items-center justify-center">
         <div className="mt-40 md:mt-0 text-center">
-          <p className="md:text-xl mb-2 md:mb-4">👋, My name is <span className="font-semibold">Muzammil Ansari</span> & I am a</p>
+          <p className="md:text-xl mb-2 md:mb-4">👋, My name is <span className="font-semibold">Muzammil Ansari</span> & I'm a</p>
           <h1>FULLSTACK<br/><span className="font-outline">DEVELOPER</span></h1>
-          <div className="flex items-center justify-between px-1">
+          <div className="md:flex items-center justify-between md:px-1">
             <p className="text-xl text-center md:text-start">based in Mumbai, India</p>
-            <div className="flex items-center justify-center md:justify-end gap-3 text-xl">
+            <div className="flex items-center justify-center md:justify-end gap-3 text-xl mt-4 md:mt-0">
               <a><RiLinkedinLine /></a>
               <a><RiGithubLine /></a>
               <a><RiFacebookLine /></a>
             </div>
+          </div>
+          <div className="flex items-center justify-center md:justify-between gap-5 mt-10">
+            <a className="btn">hire me</a>
+            <a className="btn outlined">download cv</a>
           </div>
           <div className="w-full flex items-center justify-center">
             <Image
